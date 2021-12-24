8387f9096247a1c1c78725df545b1f848366662c
emilianojan@gmail.com <emilianojan@gmail.com>
2021-12-24 10:20:24 -0300

    Add project files.

---
+++ AutoInicio/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+		Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+		Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+		Me.Label3 = New System.Windows.Forms.Label()
+		CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SplitContainer1.Panel1.SuspendLayout()
+		Me.SplitContainer1.Panel2.SuspendLayout()
+		Me.SplitContainer1.SuspendLayout()
+		Me.TableLayoutPanel1.SuspendLayout()
+		Me.TableLayoutPanel2.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'SplitContainer1
+		'
+		Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+		Me.SplitContainer1.IsSplitterFixed = True
+		Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+		Me.SplitContainer1.Name = "SplitContainer1"
+		Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+		'
+		'SplitContainer1.Panel1
+		'
+		Me.SplitContainer1.Panel1.Controls.Add(Me.TableLayoutPanel2)
+		'
+		'SplitContainer1.Panel2
+		'
+		Me.SplitContainer1.Panel2.AutoScroll = True
+		Me.SplitContainer1.Panel2.Controls.Add(Me.FlowLayoutPanel1)
+		Me.SplitContainer1.Size = New System.Drawing.Size(800, 450)
+		Me.SplitContainer1.SplitterDistance = 64
+		Me.SplitContainer1.TabIndex = 0
+		'
+		'FlowLayoutPanel1
+		'
+		Me.FlowLayoutPanel1.AutoScroll = True
+		Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+		Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+		Me.FlowLayoutPanel1.Size = New System.Drawing.Size(800, 382)
+		Me.FlowLayoutPanel1.TabIndex = 0
+		'
+		'Timer1
+		'
+		'
+		'TableLayoutPanel1
+		'
+		Me.TableLayoutPanel1.ColumnCount = 3
+		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60.0!))
+		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+		Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 0)
+		Me.TableLayoutPanel1.Controls.Add(Me.TextBox1, 1, 0)
+		Me.TableLayoutPanel1.Controls.Add(Me.Label2, 2, 0)
+		Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.TableLayoutPanel1.Location = New System.Drawing.Point(3, 3)
+		Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+		Me.TableLayoutPanel1.RowCount = 1
+		Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+		Me.TableLayoutPanel1.Size = New System.Drawing.Size(794, 40)
+		Me.TableLayoutPanel1.TabIndex = 0
+		'
+		'Label1
+		'
+		Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.Location = New System.Drawing.Point(3, 0)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(361, 40)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Delay time before starting the program"
+		Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+		Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.TextBox1.Location = New System.Drawing.Point(370, 5)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(54, 29)
+		Me.TextBox1.TabIndex = 1
+		Me.TextBox1.Text = "1"
+		'
+		'Label2
+		'
+		Me.Label2.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.Location = New System.Drawing.Point(430, 0)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(361, 40)
+		Me.Label2.TabIndex = 2
+		Me.Label2.Text = "(seconds)"
+		Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		'
+		'TableLayoutPanel2
+		'
+		Me.TableLayoutPanel2.ColumnCount = 1
+		Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+		Me.TableLayoutPanel2.Controls.Add(Me.Label3, 0, 1)
+		Me.TableLayoutPanel2.Controls.Add(Me.TableLayoutPanel1, 0, 0)
+		Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.TableLayoutPanel2.Location = New System.Drawing.Point(0, 0)
+		Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+		Me.TableLayoutPanel2.RowCount = 2
+		Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+		Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18.0!))
+		Me.TableLayoutPanel2.Size = New System.Drawing.Size(800, 64)
+		Me.TableLayoutPanel2.TabIndex = 1
+		'
+		'Label3
+		'
+		Me.Label3.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.Label3.Location = New System.Drawing.Point(3, 46)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(794, 18)
+		Me.Label3.TabIndex = 0
+		Me.Label3.Text = "Close remote desktop before time has elapsed"
+		Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.Controls.Add(Me.SplitContainer1)
+		Me.Name = "Form1"
+		Me.Text = "Auto-Start"
+		Me.SplitContainer1.Panel1.ResumeLayout(False)
+		Me.SplitContainer1.Panel2.ResumeLayout(False)
+		CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.SplitContainer1.ResumeLayout(False)
+		Me.TableLayoutPanel1.ResumeLayout(False)
+		Me.TableLayoutPanel1.PerformLayout()
+		Me.TableLayoutPanel2.ResumeLayout(False)
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents SplitContainer1 As SplitContainer
+	Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+	Friend WithEvents Timer1 As Timer
+	Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+	Friend WithEvents Label1 As Label
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents Label2 As Label
+	Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+	Friend WithEvents Label3 As Label
+End Class
---
+++ AutoInicio/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+
+    Dim Programas As New List(Of String)
+    Dim AIniciar As String
+    Dim AppDir As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Dim a As New Process
+
+        Dim proceso As New ProcessStartInfo
+        With proceso
+            .Arguments = ""
+            .FileName = "c:\Users\Public\Desktop\Arduino.lnk"
+
+        End With
+        a.StartInfo = proceso
+        a.Start()
+        'a.WaitForExit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckProgramas()
+        CargarTiempo()
+    End Sub
+
+    Private Sub CargarTiempo()
+        AppDir = AppDomain.CurrentDomain.BaseDirectory 'System.Reflection.Assembly.GetExecutingAssembly().Location
+        If My.Computer.FileSystem.FileExists(AppDir & "\gua.Date") = True Then
+            Dim valor As String = My.Computer.FileSystem.ReadAllText(AppDir & "\gua.Date")
+            TextBox1.Text = valor
+        End If
+    End Sub
+
+    Private Sub CheckProgramas()
+        Dim dire() As String = My.Computer.FileSystem.GetFiles("c:\Users\Public\Desktop").ToArray
+
+        For Each ele In dire
+            If Strings.Replace(ele, "desktop.ini", "") = ele Then
+                Dim boton As New Button
+                Dim texto As String = Strings.Replace(ele, "c:\Users\Public\Desktop\", "")
+                texto = Strings.Mid(texto, 1, Strings.Len(texto) - 4)
+
+                Dim imagen As Bitmap = System.Drawing.Icon.ExtractAssociatedIcon(ele).ToBitmap
+
+                With boton
+                    .Text = texto
+                    .Height = 60
+                    .Width = 120
+                    .Visible = True
+                    .Image = imagen
+                    .TextAlign = ContentAlignment.BottomCenter
+                    .ImageAlign = ContentAlignment.TopCenter
+                    .Tag = Programas.Count
+                End With
+                AddHandler boton.Click, AddressOf ClickBotones
+                FlowLayoutPanel1.Controls.Add(boton)
+                Programas.Add(ele)
+            End If
+        Next
+    End Sub
+
+    Private Sub ClickBotones(sender As Object, e As EventArgs)
+        Dim boto As Button = sender
+        AIniciar = Programas(boto.Tag)
+        My.Computer.FileSystem.WriteAllText(AppDir & "\gua.Date", TextBox1.Text, False)
+        Timer1.Interval = Val(TextBox1.Text) * 1000
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Enabled = False
+        Dim a As New Process
+        Dim proceso As New ProcessStartInfo
+        With proceso
+            .Arguments = ""
+            .FileName = AIniciar
+        End With
+        a.StartInfo = proceso
+        a.Start()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
